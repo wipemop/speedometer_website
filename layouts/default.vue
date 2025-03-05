ae61ff8c0338211e4e77e6a13ce3c50bb30db624
Toxxa <mindraver@googlemail.com>
2025-03-05 15:40:41 +0100

    Update default.vue
    adjusted text colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,9 +2,9 @@
   <div
       class="min-h-screen flex flex-col bg-cover bg-center"
   >
-    <header class="bg-black bg-opacity-90 p-4 text-orange-600 text-center shadow-2xl">
-      <h1 class="text-4xl font-extrabold tracking-wider">Simple Speedometer</h1>
-      <sub>by Toxxa</sub>
+    <header class="bg-black bg-opacity-90 p-4 text-center shadow-2xl">
+      <h1 class="text-4xl font-extrabold text-orange-600 tracking-wider">Simple Speedometer</h1>
+      <sub><span class="text-orange-900">by </span><span class="text-red-800">Toxxa</span> </sub>
       <a class="absolute right-5 top-5"
         href="https://github.com/wipemop/simple_speedometer">
           <img src="assets/github-mark.svg" class="max-h-[40px]">
@@ -13,7 +13,7 @@
     <main class="flex-grow">
       <NuxtPage />
     </main>
-    <footer class="bg-black bg-opacity-90 p-4 text-orange-600 text-center pb-20">
+    <footer class="bg-black bg-opacity-90 p-4 text-orange-900 text-center pb-20">
       <p class="text-sm">&copy; 2025 Toxxa</p>
     </footer>
   </div>
