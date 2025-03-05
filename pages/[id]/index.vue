9b0487829330ef616406f219dfc650c029e8d288
Toxxa <mindraver@googlemail.com>
2025-03-05 15:11:51 +0100

    Update index.vue
    reverted the failed changes

--- pages/[id]/index.vue
+++ pages/[id]/index.vue
@@ -23,18 +23,18 @@
 
         <h3 class="mt-4 font-semibold text-orange-600 text-lg">Start location:</h3>
         <div class="grid grid-cols-4 gap-2">
-          <label><span class="text-orange-600">X: </span><input type="number" v-model.number="set.Start.x" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Y: </span><input type="number" v-model.number="set.Start.y" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Z: </span><input type="number" v-model.number="set.Start.z" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Radius: </span><input class="text-white" type="number" v-model.number="set.Startradius" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>X: <input type="number" v-model.number="set.Start.x" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Y: <input type="number" v-model.number="set.Start.y" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Z: <input type="number" v-model.number="set.Start.z" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Radius: <input type="number" v-model.number="set.Startradius" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
         </div>
 
         <h3 class="mt-4 font-semibold text-orange-600 text-lg">Finish location:</h3>
         <div class="grid grid-cols-4 gap-2">
-          <label><span class="text-orange-600">X: </span><input type="number" v-model.number="set.End.x" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Y: </span><input type="number" v-model.number="set.End.y" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Z: </span><input type="number" v-model.number="set.End.z" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
-          <label><span class="text-orange-600">Radius: </span><input type="number" v-model.number="set.Endradius" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>X: <input type="number" v-model.number="set.End.x" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Y: <input type="number" v-model.number="set.End.y" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Z: <input type="number" v-model.number="set.End.z" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
+          <label>Radius: <input type="number" v-model.number="set.Endradius" class="border-b px-2 py-1 w-full bg-neutral-700"></label>
         </div>
         <h3 class="mt-4 font-semibold text-orange-600 text-lg">Checkpoint locations:</h3>
         <div @dragover.prevent @drop="dropCheckpoint(setName, $event)">
@@ -42,10 +42,10 @@
               class="mt-2 p-3 rounded-lg shadow-md grid grid-cols-4 gap-2 cursor-move border-2 border-neutral-700 relative"
               draggable="true"
               @dragstart="dragCheckpoint(setName, index, $event)">
-            <label class="py-2"><span class="text-orange-600">X: </span><input type="number" v-model.number="checkpoint.Position.x" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
-            <label class="py-2"><span class="text-orange-600">Y: </span><input type="number" v-model.number="checkpoint.Position.y" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
-            <label class="py-2"><span class="text-orange-600">Z: </span><input type="number" v-model.number="checkpoint.Position.z" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
-            <label class="py-2"><span class="text-orange-600">Radius: </span><input type="number" v-model.number="checkpoint.Radius" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
+            <label class="py-2">X: <input type="number" v-model.number="checkpoint.Position.x" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
+            <label class="py-2">Y: <input type="number" v-model.number="checkpoint.Position.y" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
+            <label class="py-2">Z: <input type="number" v-model.number="checkpoint.Position.z" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
+            <label class="py-2">Radius: <input type="number" v-model.number="checkpoint.Radius" class="border-b px-2 py-2 w-full bg-neutral-700"></label>
             <button @click="removeCheckpoint(setName, index)" class="bg-red-400 text-white px-3 py-1 rounded absolute top-2 right-3">x</button>
           </div>
         </div>
