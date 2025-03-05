7fe340abe7c035440ae29687df87abc6de6b2a0a
Toxxa <mindraver@googlemail.com>
2025-03-05 15:36:08 +0100

    Update index.vue
    adjusted the text colors

--- pages/index.vue
+++ pages/index.vue
@@ -7,17 +7,17 @@
     <!-- Main Content -->
     <main class="flex-grow container mx-auto px-4 py-12 space-y-12">
       <!-- Speed Insights Section -->
-      <div class="bg-black text-orange-600 font-bold">
+      <div class="bg-black font-bold">
         <div class="container mx-auto px-4 py-12 text-center">
           <div class="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
             <div class="bg-neutral-950 p-6 rounded-xl shadow-xl border-4 border-neutral-950">
-              <h2 class="text-3xl">Measure Your Speed</h2>
-              <p class="mt-2">Display your velocity live in-game using one of three different units on a fancy, lightly customizeable dial.</p>
+              <h2 class="text-3xl text-orange-600">Measure Your Speed</h2>
+              <p class="mt-2 text-orange-900">Display your velocity live in-game using one of three different units on a fancy, lightly customizeable dial.</p>
             </div>
 
             <div class="bg-neutral-950 p-6 rounded-xl shadow-xl border-4 border-neutral-950">
-              <h2 class="text-3xl">Choose Your Timer</h2>
-              <p class="mt-2">Run off into the sunset with no clear end, define start and finish on the fly or choose a Coordinate Set and hunt your best time!</p>
+              <h2 class="text-3xl text-orange-600">Choose Your Timer</h2>
+              <p class="mt-2 text-orange-900">Run off into the sunset with no clear end, define start and finish on the fly or choose a Coordinate Set and hunt your best time!</p>
             </div>
           </div>
 
@@ -30,7 +30,7 @@
       <!-- Online Configuration Tool Section -->
       <section class="bg-neutral-950 bg-opacity-90 p-8 rounded-lg shadow-lg">
         <h2 class="text-3xl font-bold text-orange-600 mb-4">Setup and share your own Coordinate Sets</h2>
-        <p class="text-orange-600 mb-4">
+        <p class="text-orange-900 mb-4">
           Make your own Coordinate Sets by defining start, checkpoint and finish coordinates and storing them online. Share your Sets with others and compete for best times!
           Display your selected Set ingame accurate to perspective as an overlay.
         </p>
@@ -55,7 +55,7 @@
       <section class="text-center">
         <div class="bg-neutral-950 bg-opacity-90 p-8 rounded-lg shadow-lg inline-block">
           <h2 class="text-3xl font-bold text-orange-600 mb-4">Easy Installation</h2>
-          <p class="text-orange-600 mb-4">
+          <p class="text-orange-900 mb-4">
             Get the Simple Speedometer directly in-game via the Nexus AddOn Library or download it manually from my GitHub-Repository.
           </p>
           <a class="bg-orange-600 text-white font-bold py-2 px-4 rounded hover:bg-gray-200 transition duration-300"
