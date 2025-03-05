8aecb04ec2247c733a89cafd4ac78b71b556c5b5
Toxxa <mindraver@googlemail.com>
2025-03-05 09:51:06 +0100

    Update index.vue
    tried changing bg a third time

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,21 @@
 <template>
   <div
-      class="bg-black-950 bg-opacity-90  min-h-screen flex flex-col bg-cover bg-center"
+      class="bg-black-50 bg-opacity-90  min-h-screen flex flex-col bg-cover bg-center"
       style="background-image: url('assets/image.png')"
   >
     <!-- Header Section -->
     <!-- Main Content -->
     <main class="flex-grow container mx-auto px-4 py-12 space-y-12">
       <!-- Speed Insights Section -->
-      <div class="bg-black-950 text-yellow-300 font-bold">
+      <div class="bg-black-50 text-yellow-300 font-bold">
         <div class="container mx-auto px-4 py-12 text-center">
           <div class="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
-            <div class="bg-black-800 p-6 rounded-xl shadow-xl border-4 border-black-800">
+            <div class="bg-black-150 p-6 rounded-xl shadow-xl border-4 border-black-150">
               <h2 class="text-3xl">Track Your Speed</h2>
               <p class="mt-2">Analyze your movement and improve your times in races, puzzles, and flight challenges.</p>
             </div>
 
-            <div class="bg-black-800 p-6 rounded-xl shadow-xl border-4 border-black-800">
+            <div class="bg-black-150 p-6 rounded-xl shadow-xl border-4 border-black-150">
               <h2 class="text-3xl">Custom Tracks</h2>
               <p class="mt-2">Define start & endpoints, set checkpoints, and measure your best runs!</p>
             </div>
@@ -28,7 +28,7 @@
       </div>
 
       <!-- Online Configuration Tool Section -->
-      <section class="bg-black-800 bg-opacity-90 p-8 rounded-lg shadow-lg">
+      <section class="bg-black-150 bg-opacity-90 p-8 rounded-lg shadow-lg">
         <h2 class="text-3xl font-bold text-yellow-300 mb-4">Custom Track Configuration</h2>
         <p class="text-yellow-300 mb-4">
           Easily set up your custom tracks by defining start and endpoint locations, plus all the essential checkpoints you must hit.
@@ -53,7 +53,7 @@
 
       <!-- Installation Section -->
       <section class="text-center">
-        <div class="bg-black-800 bg-opacity-90 p-8 rounded-lg shadow-lg inline-block">
+        <div class="bg-black-150 bg-opacity-90 p-8 rounded-lg shadow-lg inline-block">
           <h2 class="text-3xl font-bold text-yellow-300 mb-4">Easy Installation</h2>
           <p class="text-yellow-300 mb-4">
             Install quickly via the Nexus Plugin Loader and get racing in minutes.
